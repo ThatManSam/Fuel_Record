89ab7f6728227f9518a1ddf34224c53eca749f51
Joel Shepherd <joeldamole0@gmail.com>
2021-08-13 23:37:18 +1200

    Added upto toggle complete (next is delete)

--- react-crash-couse/src/App.js
+++ react-crash-couse/src/App.js
@@ -1,11 +1,47 @@
+import React, { Component } from 'react';
 import './App.css';
+import Todos from './components/Todos';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>This is a cool app</h1>
-    </div>
-  );
+class App extends Component {
+    state = {
+        todos: [
+            {
+                id: 1,
+                title: 'Take out the trash',
+                completed: false
+            },
+            {
+                id: 2,
+                title: 'Mow the Lawn',
+                completed: false
+            },
+            {
+                id: 3,
+                title: 'Submit assignment',
+                completed: false
+            },
+        ]
+    }
+
+    // Toggle Complete
+    markComplete = (id) => {
+        this.setState({
+            todos: this.state.todos.map(todo => {
+                if (todo.id === id) {
+                    todo.completed = !todo.completed;
+                }
+                return todo;
+            })
+        });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Todos todos={this.state.todos} markComplete={this.markComplete} />
+            </div>
+        );
+    }
 }
 
 export default App;
