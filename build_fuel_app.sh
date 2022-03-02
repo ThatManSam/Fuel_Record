83d4fa2d7ac94f97cced771308ea3e983d7cca1b
Joel Shepherd <joeldamole0@gmail.com>
2022-03-03 00:32:11 +1300

    Fixed build script copy location

--- build_fuel_app.sh
+++ build_fuel_app.sh
@@ -2,4 +2,4 @@
 cd fuel-app/ || echo "Could change to dir fuel-app/"
 npm run-script build || echo "Error running 'npm run-script build'"
 cd ../
-cp -r fuel-app/build/ fuel-server/ || echo "Couldn't copy build file"
+cp -r fuel-app/build/ fuel-server/build || echo "Couldn't copy build file"
